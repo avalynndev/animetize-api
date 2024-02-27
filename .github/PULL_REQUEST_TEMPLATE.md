a27fe3213d0e6dd9065ce09ca2fc1df011f09c90
Avalynn <avalynndev@gmail.com>
2024-02-27 13:42:20 +0530

    push changes v0.0.1

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,5 @@
+## Summary
+
+Describe your changes here.
+
+Fixes #Issue_Number, dotnet/docs#Issue_Number or dotnet/dotnet-api-docs#Issue_Number (if available)
---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import Fastify from 'fastify';
+import FastifyCors from '@fastify/cors';
+
+//import anime from "./scrapper/anime";
+//import manga from "./scrapper/manga";
+//import anilist from "./scrapper/anilist";
+import chalk from 'chalk';
+
+// Create a new Fastify instance
+const fastify = Fastify({
+  maxParamLength: 1000,
+  logger: true,
+});
+
+// Register the CORS plugin with Fastify
+(async () => {
+  const PORT = 3000;
+
+  await fastify.register(FastifyCors, {
+    origin: '*',
+    methods: 'GET',
+  });
+
+  console.log(chalk.green(`Starting server on port ${PORT}... 🚀`));
+
+  // Register the anime, manga, and anilist routes
+  // await fastify.register(anime, { prefix: "/anime" });
+  // await fastify.register(manga, { prefix: "/manga" });
+  // await fastify.register(anilist, { prefix: "/anilist" });
+
+  try {
+    // Define the root route
+    fastify.get('/', (_, rp) => {
+      rp.status(200).send(`Welcome to Animetize api! 🎉`);
+    });
+
+    // Define the catch-all route for handling 404 errors
+    fastify.get('*', (request, reply) => {
+      reply.status(404).send({
+        message: '',
+        error: '404 Error: Page not found',
+      });
+    });
+
+    // Start the Fastify server
+    fastify.listen({ port: PORT, host: '0.0.0.0' }, (e, address) => {
+      if (e) throw e;
+      console.log(`Server listening on ${address}`);
+    });
+  } catch (err: any) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+})();
+
+// Export the handler function for use with serverless platforms
+export default async function handler(req: any, res: any) {
+  await fastify.ready();
+  fastify.server.emit('request', req, res);
+}
